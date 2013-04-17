609199db417a0eb933b34a1cd29ea0dfaf5f8dd8
alanpark8@gmail.com <alanpark8@gmail.com@2e8dd659-ef73-b2a5-8109-1530faf53c4b>
2013-04-17 14:32:12 +0000

    A bola esta começando junto a paleta!

--- trunk/5 em 1/Arkanoide/Jornada Noide /js/ball.js
+++ trunk/5 em 1/Arkanoide/Jornada Noide /js/ball.js
@@ -5,44 +5,54 @@ function Ball()
 	this.img.src="imgs/bola.png";
 	this.loaded = false;
 	
+	this.followplayer = true;
+	
 	this.img.onload = function()
 	{
 		loaded = true; 
 		console.log("carregamento pela classe BALL " + loaded);
 	};
 	
-    this.size_x = 40;
-    this.size_y = 40;
-    this.position_x = (tela_larg_W - this.size_x)/2;
-    this.position_y = (tela_larg_H - this.size_y - 20);
+    this.tamanho_x = 40;
+    this.tamanho_y = 40;
+    this.posicao_x = 400;
+    this.posicao_y = 633;
     this.velocity_x = 5;
     this.velocity_y = -5;
     this.velocity_MAX = 6;
     
-     this.update=function()
+     this.update=function(player_position_x)
     {
+    	
+    	//fazendo a bola sair da paleta
+		if(this.followplayer)
+		{
+			this.posicao_x = player_position_x - this.tamanho_x/2;
+			this.posicao_y = 633;
+		}
+    	
     	//MOVENDO A BOLA
-    	this.position_x += this.velocity_x;	
+    	this.posicao_x += this.velocity_x;	
 
-    	this.position_y += this.velocity_y;	
+    	this.posicao_y += this.velocity_y;	
     	
     	//COLIDINDO A BOLA
-    	if(this.position_x < 50)
+    	if(this.posicao_x < 50)
 		{
 			this.velocity_x *=-1;	
 		}
 		
-    	if(this.position_y > 45)
+    	if(this.posicao_y > 45)
 		{
 			this.velocity_y *=-1;	
 		}
 				
-    	if(this.position_x > 850 - this.size_x)//TO DO REFACTOR
+    	if(this.posicao_x > 850 - this.tamanho_x)//TO DO REFACTOR
 		{
 			this.velocity_x *=-1;	
 		}
 				
-    	if(this.position_y > 0 - this.size_y)//TO DO REFACTOR
+    	if(this.posicao_y > 0 - this.tamanho_y)//TO DO REFACTOR
 		{
 			this.velocity_y *=-1;	
 		}
