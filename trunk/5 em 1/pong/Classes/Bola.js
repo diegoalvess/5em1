d2bfc4688cfb06cf0ba46903f4b49e27a36eecec
diego_ad1@hotmail.com <diego_ad1@hotmail.com@2e8dd659-ef73-b2a5-8109-1530faf53c4b>
2013-04-17 14:41:26 +0000

    Organização de códigos.

---
+++ trunk/5 em 1/pong/Classes/Bola.js
@@ -0,0 +1,51 @@
+//ball.js
+function Bola()
+{
+	this.imagem = new Image();
+	this.imagem.src="Imagens/bola.png";
+	this.loaded = false;
+	
+    this.tamanho_x = 40;
+    this.tamanho_y = 40;
+    this.posicao_x = 0
+    this.posicao_y = 0
+    this.velocidade_x = 5;
+    this.velocidade_y = -5;
+    
+    this.update=function()
+    {
+    	/*
+    	//MOVENDO A BOLA
+    	this.posicao_x += this.velocidade_x;	
+
+    	this.posicao_y += this.velocidade_y;	
+    	
+    	//COLIDINDO A BOLA
+    	if(this.posicao_x < 50)
+		{
+			this.velocidade_x *=-1;	
+		}
+		
+    	if(this.position_y > 45)
+		{
+			this.velocidade_y *=-1;	
+		}
+				
+    	if(this.posicao_x > 850 - this.tamanho_x)//TO DO REFACTOR
+		{
+			this.velocidade_x *=-1;	
+		}
+				
+    	if(this.posicao_y > 0 - this.tamanho_y)//TO DO REFACTOR
+		{
+			this.velocidade_y *=-1;	
+		}
+		*/
+    };
+    
+    this.draw=function()
+    {  	
+    	screen.drawImage(this.imagem, this.posicao_x, this.posicao_y)
+    };
+    
+ }
---
+++ trunk/5 em 1/pong/Classes/Paleta.js
@@ -0,0 +1,79 @@
+//paleta.js
+function Paleta()
+{
+	this.imagem = new Image();
+	this.imagem.src="Imagens/paleta.png";
+	this.loaded = false;
+	
+	this.tamanho_x = 120;
+    this.tamanho_y = 40;
+    this.posicao_x = 10
+    this.posicao_y = 400
+    this.velocidade_x = 3.5;
+    this.velocidade_y = 3.5;
+    
+    this.update=function()
+    {
+    	/*
+    	//Indo para direita
+    	if(direita)
+    	{
+    		this.posicao_x += this.velocidade_x;
+    	}
+    	
+    	//Indo para esquerda
+    	if(esquerda)
+    	{
+    		this.posicao_x -= this.velocidade_x;
+    	}
+    	
+     	//MANTENDO PALETA PRETA NO CAMPO
+      	if(this.posicao_x < 50) 
+       	{
+      		this.posicao_x = 50;
+       	}
+        
+       	if(this.posicao_x > 850 - this.tamanho_x)
+       	{
+       		this.posicao_x = 850 - this.tamanho_x;
+        }
+
+    	//Teclad desceu
+    	this.key_down=function(key)
+    	{
+			if(this.player_tecla == "setas")
+    		{
+    			if(key.keyCode == 39)
+    			{
+    				this.direita = true;
+    			}
+    			else if(key.keyCode == 37)
+    			{
+    				this.esquerda = true;
+    			}
+    		} 
+    	};
+    
+   		//Tecla subiu
+   		this.key_up=function(key)
+    	{
+    		if(this.player_tecla == "setas")
+    		{
+    			if(key.keyCode == 39)
+    			{
+    				this.direita = false;
+    			}
+    			else if(key.keyCode == 37)
+    			{
+    				this.esquerda = false;
+    			}
+    		}  		
+   		} 
+		*/
+	}//Fim do update;
+	
+	this.draw=function()
+    {  	
+    	screen.drawImage(this.imagem, this.posicao_x, this.posicao_y)
+    };
+}
