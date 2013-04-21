6a6145bb363d9f9c021cb8b19b97ca3bb2670c40
diego_ad1@hotmail.com <diego_ad1@hotmail.com@2e8dd659-ef73-b2a5-8109-1530faf53c4b>
2013-04-21 14:53:32 +0000

    colocando Paleta.js para funcionar e Bola.js para andar no campo.

--- trunk/5 em 1/pong/Classes/Paleta.js
+++ trunk/5 em 1/pong/Classes/Paleta.js
@@ -1,79 +1,116 @@
 //paleta.js
-function Paleta()
+function Paleta(player_tecla)
 {
+	this.player_tecla = player_tecla;
+
 	this.imagem = new Image();
 	this.imagem.src="Imagens/paleta.png";
 	this.loaded = false;
 	
-	this.tamanho_x = 120;
-    this.tamanho_y = 40;
+	this.tamanho_x = 40;
+    this.tamanho_y = 120;
     this.posicao_x = 10
     this.posicao_y = 400
-    this.velocidade_x = 3.5;
-    this.velocidade_y = 3.5;
+    this.velocidade_x = 0;
+    this.velocidade_y = 10;
+	
+	//Teclas
+	this.sobe = false;
+	this.desce = false;
     
+	
     this.update=function()
     {
-    	/*
-    	//Indo para direita
-    	if(direita)
-    	{
-    		this.posicao_x += this.velocidade_x;
-    	}
-    	
-    	//Indo para esquerda
-    	if(esquerda)
-    	{
-    		this.posicao_x -= this.velocidade_x;
-    	}
-    	
-     	//MANTENDO PALETA PRETA NO CAMPO
-      	if(this.posicao_x < 50) 
-       	{
-      		this.posicao_x = 50;
-       	}
-        
-       	if(this.posicao_x > 850 - this.tamanho_x)
-       	{
-       		this.posicao_x = 850 - this.tamanho_x;
-        }
-
-    	//Teclad desceu
-    	this.key_down=function(key)
-    	{
-			if(this.player_tecla == "setas")
-    		{
-    			if(key.keyCode == 39)
-    			{
-    				this.direita = true;
-    			}
-    			else if(key.keyCode == 37)
-    			{
-    				this.esquerda = true;
-    			}
-    		} 
-    	};
-    
-   		//Tecla subiu
-   		this.key_up=function(key)
-    	{
-    		if(this.player_tecla == "setas")
-    		{
-    			if(key.keyCode == 39)
-    			{
-    				this.direita = false;
-    			}
-    			else if(key.keyCode == 37)
-    			{
-    				this.esquerda = false;
-    			}
-    		}  		
-   		} 
-		*/
-	}//Fim do update;
+		//Movendo paleta
+		this.movendo();
+		
+		//Colidindo com as bordas
+		this.saindodatela();
+	}
 	
 	this.draw=function()
     {  	
     	screen.drawImage(this.imagem, this.posicao_x, this.posicao_y)
     };
+	
+    //movendo player
+    this.movendo=function()
+    {
+    	if(this.direita)
+    	{
+    		this.posicao_x += this.velocidade_x;
+    	}
+    	if(this.esquerda)
+    	{
+    		this.posicao_x -= this.velocidade_x;
+    	}
+    	if(this.sobe)
+    	{
+    		this.posicao_y -= this.velocidade_y;
+    	}
+    	if(this.desce)
+    	{
+    		this.posicao_y += this.velocidade_y;
+    	}
+    }
+    
+	//Colisão para não sair da tela
+    this.saindodatela=function()
+    {
+    	//saida pela direita
+    	if(this.posicao_x < 0)
+    	{
+    		this.posicao_x = 0;
+    	}
+    	
+    	//saida por cima
+    	if(this.posicao_y < 0)
+    	{
+    		this.posicao_y = 0;
+    	}
+    	
+    	//saida pela esquerda
+    	if(this.posicao_x > tela_tamanho_x - this.tamanho_x)
+    	{
+    		this.posicao_x = tela_tamanho_x - this.tamanho_x;
+    	}
+    	
+    	//saida por baixo
+    	if(this.posicao_y > tela_tamanho_y - this.tamanho_y)
+    	{
+    		this.posicao_y = tela_tamanho_y- this.tamanho_y;
+    	}
+    }
+    
+    //Tecla desceu
+    this.key_down=function(key)
+    {
+		if(this.player_tecla == "setas")
+    	{
+    		if(key.keyCode == 38)
+    		{
+    			this.sobe = true;
+    		}
+    		else if(key.keyCode == 40)
+    		{
+    			this.desce = true;
+    		}	
+    	} 
+    };
+    
+    //Tecla subiu
+    this.key_up=function(key)
+    {
+    	if(this.player_tecla == "setas")
+    	{
+    		if(key.keyCode == 38)
+    		{
+    			this.sobe = false;
+    		}
+    		else if(key.keyCode == 40)
+    		{
+    			this.desce = false;
+    		}   			
+    	}  		
+    }	
 }
