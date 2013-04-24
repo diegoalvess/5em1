9e1b92a9109bc12b3de9aaac9cade7df3954e3d6
diego_ad1@hotmail.com <diego_ad1@hotmail.com@2e8dd659-ef73-b2a5-8109-1530faf53c4b>
2013-04-24 15:16:33 +0000

    alteracoes de codigo

---
+++ trunk/5 em 1/invaders/Classes/bloco.js
@@ -0,0 +1,59 @@
+function Bloco(source, hp)
+{
+	this.imagem = new Image();
+	this.imagem.src="Imagens/Bloco.png"
+	this.carregada = false;
+	this.visible = true; //BLOCO FICA VISIVEL//
+	this.hp = hp; //ENERGIA DO BLOCO//
+	this.pontos = this.hp*2;
+		
+	//Fazendo inimigos descerem na tela
+    var godownandreversedirection = false;
+	
+	this.imagem.onload = function() 
+	{
+		carregada = true; 
+	};
+	
+	this.tamanho_x = 50;
+	this.tamanho_y = 50;
+	this.posicao_x = 100;
+	this.posicao_y = 60;
+	this.velocidade_x = 5;
+	this.velocidade_y = 0;
+	
+	this.update=function()
+	{
+		this.posicao_y += this.velocidade_y;
+		this.posicao_x += this.velocidade_x;
+		
+		//Colidindo com as bordas
+		this.saindodatela();		
+	};
+	
+	this.draw=function()
+    {  	
+		if(this.visible)
+    	screen.drawImage(this.imagem, this.posicao_x, this.posicao_y)
+    };
+    
+    this.saindodatela=function()
+    {
+    	//saida pela esquerda
+    	if(this.posicao_x < 0)
+    	{
+    		this.posicao_x = 0;
+    		//godownandreversedirection = false;
+    	}
+    	
+    	//saida pela direita
+    	if(this.posicao_x > tela_tamanho_x - this.tamanho_x)
+    	{
+    		this.posicao_x = tela_tamanho_x - this.tamanho_x;
+    		//godownandreversedirection = false;
+    	}
+    }
+    
+    
+  
+}
---
+++ trunk/5 em 1/invaders/Classes/scenelevel1.js
@@ -0,0 +1,93 @@
+function SceneLevel1()
+{
+	//Player
+	var player = new Player("setas");	
+		
+	//Blocos
+	var blocos = new Bloco();
+	
+	for(var i = 0; i < 10; i++)
+    {	
+        blocos[i] = new Bloco("imagens/bloco.png");
+        blocos[i].posicao_y = 100        	
+        blocos[i].posicao_x = 200 + (i * (blocos[i].tamanho_w + 10 ));  	
+    }
+
+	//Pause
+	var paused = true;
+   
+    //Colocando a imagem de background
+    var background = new Background("Imagens/FundoBackground.png");
+	
+    //Efeito sonoro
+    //this.efeito_sonoro = new Audio();
+    //this.efeito_sonoro.src = "Sons/Som1.mp3"
+    //this.efeito_sonoro.load();
+    
+    this.update=function()
+    {
+    	//Pause
+    	if(!paused)
+    	{
+    		return;  	
+    	}
+		
+		//Som
+    	//this.efeito_sonoro.play();
+    	
+    	//Background se movendo
+    	background.update();
+    	
+    	//Player
+    	player.update();
+    	
+		//Bloco
+		blocos.update();			
+    };
+    
+    this.draw=function()
+    { 	
+    	//Desenhando fundo
+    	background.draw();
+    	
+    	//Desenhando player
+    	player.draw();
+    	
+		//Desenhando bloco
+		blocos.draw();
+    };
+    
+    this.mouse_down=function(mouse)
+    {
+    	//currentScene = SCENE.FIMGANHOU;
+    	//this.efeito_sonoro.pause();
+    };
+     
+ 	 //Tecla desceu
+    this.key_down=function(key)
+    {
+		player.key_down(key);
+    };
+    
+    //Tecla subiu
+    this.key_up=function(key)
+    {
+    	player.key_up(key);
+	};	
+	
+	//Função de colisão
+	function Collide(x1, y1, w1, h1, x2, y2, w2, h2)
+	{
+		if((x1+w1<x2) //SE RECT1 ESQUERDO RECT2//
+			||(x1>x2+w2) //SE RECT1 DIREITO RECT2//
+			||(y1+h1<y2) //SE RECT1 CIMA RECT2//
+			||(y1>y2+h2)) //SE RECT1 BAIXO RECT2//
+		{
+			return false; //NÃO COLIDIU//
+		}
+		else	
+		{
+			return true; //COLIDIU//
+		}
+	}	
+}
