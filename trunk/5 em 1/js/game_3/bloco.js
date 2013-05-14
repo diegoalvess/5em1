ed1d29dae1e2bb366531c359fedc57371a5dac2f
pablomarques10@gmail.com <pablomarques10@gmail.com@2e8dd659-ef73-b2a5-8109-1530faf53c4b>
2013-05-14 15:00:24 +0000

    Adicionando js game3

---
+++ trunk/5 em 1/js/game_3/bloco.js
@@ -0,0 +1,85 @@
+function Bloco(source, hp)
+{
+	this.imagem = new Image();
+	this.imagem.src= source;
+	this.carregada = false;
+	this.visible = true;
+	this.hp = hp;
+	this.pontos = this.hp*2;
+		
+	//Fazendo inimigos descerem na tela
+    var godownandreversedirection = false;
+	
+	this.imagem.onload = function() 
+	{
+		carregada = true; 
+	};
+	
+	this.tamanho_x = 50;
+	this.tamanho_y = 50;
+	this.posicao_x = 100;
+	this.posicao_y = 60;
+	this.velocidade_x = 5;
+	this.velocidade_y = 0;
+	
+	this.update=function()
+	{
+		//Fazendo blocos andarem
+		this.posicao_y += this.velocidade_y;
+		this.posicao_x += this.velocidade_x;
+		
+		//Colidindo com as bordas
+		this.saindodatela();		
+	};
+	
+	this.draw=function()
+    {  	
+		if(this.visible)
+    	screen.drawImage(this.imagem, this.posicao_x, this.posicao_y)
+    };
+    
+    this.saindodatela=function()
+    {
+    	//saida pela esquerda
+    	if(this.posicao_x < 0)
+    	{
+    		this.posicao_x = 0;
+    		godownandreversedirection = false;
+    	}
+    	
+    	//saida pela direita
+    	if(this.posicao_x > tela_tamanho_x - this.tamanho_x)
+    	{
+    		this.posicao_x = tela_tamanho_x - this.tamanho_x;
+    		godownandreversedirection = true;
+    	}
+    
+
+	
+	//dizendo o que a variavel de descer e inverter os inimigos vai fazer	
+	if(godownandreversedirection == true)
+	{
+		//
+		{
+			this.posicao_y += 20;
+			
+			if(this.velocidade_x > 0)
+			{
+				this.velocidade_x++;
+			}
+			else
+			{
+				this.velocidade_x--;	
+			}
+			
+			this.velocidade_x *= -1;
+			
+			godownandreversedirection = false;
+
+		}
+	}
+	
+    }
+
+  
+}
---
+++ trunk/5 em 1/js/game_3/menu.js
@@ -0,0 +1,73 @@
+function SceneMenu()
+{
+	this.imagem = new Image();//BACKGROUND
+	this.imagem.src = "Imagens/Menu.png"
+	this.loaded = false;
+    
+    this.tamanho_x = 1500;
+    this.tamanho_y = 1366;
+
+    this.posicao_x = 0;
+    this.posicao_y = 0;
+    
+    //Som
+    //this.efeito_sonoro = new Audio();
+    //this.efeito_sonoro.src = "Sons/Menu.mp3"
+    //this.efeito_sonoro.load();
+    
+    //imagem dos botões
+    //this.botao_comecar = new Button("Imagens/botaocomecar.png", 302, 85, 580, 300);
+    //this.botao_creditos = new Button("Imagens/botaocredito.png", 302, 85, 630, 480);
+    //this.botao_instru = new Button("Imagens/botaoinstrucoes.png", 302, 85, 400, 550);
+    
+    this.imagem.onload = function()
+	{
+		loaded = true; 
+	};
+    
+    this.update=function()
+    {
+    	//this.efeito_sonoro.play();
+    };
+    
+    this.draw=function()
+    {  	
+    	screen.drawImage(this.imagem, 0, 0);
+    	
+    	//desenho dos botões
+    	//this.botao_comecar.draw();
+    	//this.botao_creditos.draw(); 	
+    };
+    
+    
+    this.mouse_down=function(mouse)
+    {   	
+    	//para mudar da tela pra algum lugar coloque o comando aqui	
+    	currentScene = SCENE.LEVEL1;
+    	
+    	//função do clique para o botão mudar de cena
+    	/*if(this.botao_comecar.clicou(mouse))
+    	{
+    		currentScene = SCENE.INTRODUCAO;
+    		this.efeito_sonoro.pause();	
+    	}
+    	
+    	if(this.botao_creditos.clicou(mouse))
+    	{
+    		currentScene = SCENE.CREDITOS;		
+    	}
+    	*/
+    };
+    
+    
+    this.key_down=function(key)
+    {
+		
+    };
+    
+    this.key_up=function(key)
+    {
+		
+    };
+    
+}
