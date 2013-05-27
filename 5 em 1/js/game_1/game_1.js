4fee7e93b507ddeb32ae957d47dbffa379c8669b
diego_ad1 <diego_ad1@hotmail.com>
2013-05-27 22:38:48 +0000

    Fazendo colisão com blocos, bola, paletas (inimigo e player) e organização de código.

--- 5 em 1/js/game_1/game_1.js
+++ 5 em 1/js/game_1/game_1.js
@@ -1,200 +1,92 @@
 function Game_1()
 {	
-		//Pausa
-		//this.paused = false;
+	//Pausa
+	//this.paused = false;
 		
-		//Introducao
-		this.introducao = new Game1SceneIntroducao();
+	//Introducao
+	this.introducao = new Game1SceneIntroducao();
 
-		//Level
-		this.level1 = new Game1SceneLevel1();
+	//Level
+	this.level1 = new Game1SceneLevel1();
 		
-		//FimGanhou 
-		this.fimganhou = new Game1SceneGanhou();
+	//FimGanhou 
+	this.fimganhou = new Game1SceneGanhou();
 		
-		//FimPerdeu
-		this.fimperdeu = new Game1ScenePerdeu();
+	//FimPerdeu
+	this.fimperdeu = new Game1ScenePerdeu();
 	
-		//Fluxo das telas
-		this.SCENE = { INTRODUCAO: 0, LEVEL_1: 1, FIMGANHOU: 2, FIMPERDEU: 3}
+	//Fluxo das telas
+	this.SCENE = { INTRODUCAO: 0, LEVEL_1: 1, FIMGANHOU: 2, FIMPERDEU: 3}
 				
-		//Para mudar aonde o jogo comeca, ou seja qual cena, mude aqui
-		this.currentScene = this.SCENE.INTRODUCAO;
+	//Para mudar aonde o jogo comeca, ou seja qual cena, mude aqui
+	this.currentScene = this.SCENE.INTRODUCAO;
 
-		this.update=function()
-		{	
-			switch(this.currentScene)
-			{
-				//introducao
-				case this.SCENE.INTRODUCAO:
-				{
-					this.introducao.update();		
-				}
-				break;
-								
-				//level1
-				case this.SCENE.LEVEL_1:
-				{
-					this.level1.update();
-				}
-				break;
-				
-				//FimGanhou
-				case this.SCENE.FIMGANHOU:
-				{
-					this.fimganhou.update();
-				}
-				break;
-				
-				//FimPerdeu
-				case this.SCENE.FIMPERDEU:
-				{
-					this.fimperdeu.update();
-				}
-				break;
-				
-				
-			}//fechar switch
-			
-		}//fechar update
-
-		this.draw=function()
+	this.update=function()
+	{	
+		switch(this.currentScene)
 		{
-			switch(this.currentScene)
+			//introducao
+			case this.SCENE.INTRODUCAO:
 			{
-				case this.SCENE.INTRODUCAO:
-				{
-					this.introducao.draw();
-				}
-				break;
-						
-				case this.SCENE.LEVEL_1:
-				{
-					this.level1.draw();
-				}
-				break;
-				
-				case this.SCENE.FIMGANHOU:
-				{
-					this.fimganhou.draw();
-				}
-				break;
-				
-				case this.SCENE.FIMPERDEU:
-				{
-					this.fimperdeu.draw();
-				}
-				break;
-				
+				this.introducao.update();		
 			}
-			
-		}
-
-		//Funcionando teclado e mouse.
-		this.mouse_down = function(mouse)
-		{
-			switch(this.currentScene)
-			{
-				case this.SCENE.INTRODUCAO:
-				{
-					this.introducao.mouse_down(mouse);
-				}
-				break;
-				
-				case this.SCENE.LEVEL_1:
-				{
-					this.level1.mouse_down(mouse);
-				}
-				break;
-				
-				case this.SCENE.FIMGANHOU:
-				{
-					this.fimganhou.mouse_down(mouse);
-				}
-				break;
-				
-				case this.SCENE.FIMPERDEU:
-				{
-					this.fimperdeu.mouse_down(mouse);
-				}
-				break;
-								
-			}
-		}		
+			break;
 		
-		this.key_down = function(key)
-		{
-			switch(this.currentScene)
+			//level1
+			case this.SCENE.LEVEL_1:
 			{
-				case this.SCENE.INTRODUCAO:
-				{
-					this.introducao.key_down(key);
-				}
-				break;
+				this.level1.update();
+			}
+			break;
+			
+			//FimGanhou
+			case this.SCENE.FIMGANHOU:
+			{
+				this.fimganhou.update();
+			}
+			break;
+			
+			//FimPerdeu
+			case this.SCENE.FIMPERDEU:
+			{
+				this.fimperdeu.update();
+			}
+			break;
 				
-				case this.SCENE.LEVEL_1:
-				{
+				
+		}//fechar switch
+			
+	}//fechar update
 
-					this.level1.key_down(key);
-				}
-				break;
-				
-				case this.SCENE.FIMGANHOU:
-				{
-					this.fimganhou.key_down(key);
-				}
-				break;
-				
-				case SCENE.FIMPERDEU:
-				{
-					this.fimperdeu.key_down(key);
-				}
-				break;
-								
-			}
-		}
-				
-		this.key_up = function(key)
-		{
-			
-			switch(this.currentScene)
-			{
-				case this.SCENE.INTRODUCAO:
-				{
-					this.introducao.key_up(key);
-				}
-				break;
-				
-				case this.SCENE.LEVEL_1:
-				{
-					this.level1.key_up(key);
-				}
-				break;
-				
-				case this.SCENE.FIMGANHOU:
-				{
-					this.fimganhou.key_up(key);
-				}
-				break;
-				
-				case SCENE.FIMPERDEU:
-				{
-					this.fimperdeu.key_up(key);
-				}
-				break;
-								
-			}
-		}
-			
-/*
 	this.draw=function()
 	{
-		screen.font = "20px Comic Sans MS";
-		screen.fillStyle="#000000";
-		screen.fillText("GAME_1", 20, 20);
-		
+		switch(this.currentScene)
+		{
+			case this.SCENE.INTRODUCAO:
+			{
+				this.introducao.draw();
+			}
+			break;
+				
+			case this.SCENE.LEVEL_1:
+			{
+				this.level1.draw();
+			}
+			break;
+				
+			case this.SCENE.FIMGANHOU:
+			{
+				this.fimganhou.draw();
+			}
+			break;
+				
+			case this.SCENE.FIMPERDEU:
+			{
+				this.fimperdeu.draw();
+			}
+			break;
+				
+		}
+			
 	}
-	
-*/
-
 }
