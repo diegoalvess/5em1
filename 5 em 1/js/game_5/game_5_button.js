4cdf5aba13cd921a257c340d9f60799d45b83ede
diego_ad1 <diego_ad1@hotmail.com>
2013-05-31 21:50:09 +0000

    "Colocando " o quinto jogo mas com um aviso que está em fase de construção.

---
+++ 5 em 1/js/game_5/game_5_button.js
@@ -0,0 +1,51 @@
+function Game5Button(arquivo, tam_x, tam_y, pos_x, pos_y)
+{
+	this.imagem = new Image();
+	this.imagem.src = arquivo;
+	this.loaded = false;
+	
+	//tamanho
+	this.tamanho_x = tam_x;
+    this.tamanho_y = tam_y;
+    this.posicao_x = pos_x;
+    this.posicao_y = pos_y;
+
+	this.imagem.onload = function()
+	{
+		loaded = true; 
+	};
+    
+    this.update=function()
+    {
+    	
+    };
+    
+    this.draw=function()
+    {  	
+    	//desenho do botão e sua respectiva posição
+    	screen.drawImage(this.imagem, this.posicao_x, this.posicao_y)
+    };
+    
+   	//variável de clique do botão
+    this.clicou = function(mouse)
+    {
+    	
+    	//colisão
+    	if(Collide(
+    		mouse.x,
+    		mouse.y,
+    		1,
+    		1,
+    		this.posicao_x,
+    		this.posicao_y,
+    		this.tamanho_x,
+    		this.tamanho_y
+    	))
+    	{
+    		
+    		return true;
+    	}
+    	
+    	return false;
+    };
+}
---
+++ 5 em 1/js/game_5/game_5_scene_intro.js
@@ -0,0 +1,66 @@
+function Game5SceneIntroducao()
+{
+	this.imagem = new Image();
+	this.imagem.src = "imgs/game_5/Introducao.png"
+	this.loaded = false;
+    this.tamanho_x = 800;
+    this.tamanho_y = 600;
+    this.posicao_x = 0;
+    this.posicao_y = 0;
+    
+    //Imagem dos botoes							TAMANHO_X,TAMANHO_Y,POSICAO_X,POSICAO_Y
+    //this.botaoplay = new Game5Button("imgs/game_5/play.png", 248, 98, 550, 300 );
+    this.botaoback = new Game5Button("imgs/game_5/back.png", 250, 97, 310, 525 ); 
+    
+    //Efeito Sonoro
+	this.efeito_sonoro = new Audio();
+    this.efeito_sonoro.src = "sounds/game_5/Efeitomenu.wav"
+    this.efeito_sonoro.load();
+    
+    this.imagem.onload = function()
+	{
+		loaded = true; 
+	};
+    
+    this.update=function()
+    {
+    	
+    };
+    
+    this.draw=function()
+    {  	
+    	//Desenhando fundo da intro
+    	screen.drawImage(this.imagem, 0, 0)
+    	
+    	//Desenhando botoes
+    	//this.botaoplay.draw();
+    	this.botaoback.draw();
+    };
+    
+    this.mouse_down=function(mouse)
+    {   
+    	//para mudar da tela pra algum lugar coloque o comando aqui
+    	/*if(this.botaoplay.clicou (mouse))
+    	{
+    		game_5.currentScene = game_5.SCENE.LEVEL_1;
+    		this.efeito_sonoro.play(); 		
+    	}
+		*/
+    	
+    	if(this.botaoback.clicou (mouse))
+    	{
+    		currentScene = SCENE.INTRO;
+    		this.efeito_sonoro.play(); 
+    	}	
+    };
+     
+    this.key_down=function(key)
+    {
+		
+    };
+    
+	this.key_up=function(key)
+    {
+		
+    };
+}
